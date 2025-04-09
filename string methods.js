69e3177444b0e4438a14016b2cb15c2ff2f0c54f
Praveennaik6 <praveennaik0814@gmail.com>
2025-04-09 10:46:41 +0530

    string file

---
+++ string methods.js
@@ -0,0 +1,13 @@
+// String Concatination
+
+let s="java"
+let n="Script"
+console.log(s+n) // using '+' symbol
+
+let s1=s.concat(n) //using concat built in function
+console.log(s1)  
+
+
+let s2="java".concat(n)
+console.log(s2)
+
