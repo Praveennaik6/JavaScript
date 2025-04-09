c5308a55e11dde161ee6845d37cd366e8cb3d7df
Praveennaik6 <praveennaik0814@gmail.com>
2025-04-09 11:58:56 +0530

    js file changed

--- string methods.js
+++ string methods.js
@@ -1,13 +1,57 @@
 // String Concatination
 
-let s="java"
-let n="Script"
-console.log(s+n) // using '+' symbol
+// let s="java"
+// let n="Script"
+// console.log(s+n) // using '+' symbol
 
-let s1=s.concat(n) //using concat built in function
-console.log(s1)  
+// let s1=s.concat(n) //using concat built in function
+// console.log(s1)  
 
 
-let s2="java".concat(n)
-console.log(s2)
+// let s2="java".concat(n) 
+// console.log(s2)
+
+//  s=s.slice(1)
+// console.log(s)
+
+
+
+// let s="cat"
+// let n="bat"
+
+// // s=s.replace("c","b")
+
+// s="b"+s.slice(1 )
+// console.log(s)
+
+
+// let userinput=" Rohan | ROHAN@gmail.com | javascript develpoer"
+// let s=userinput.split('|')
+// console.log(s)
+
+// let s1=userinput.trim()
+// console.log(s1)
+
+
+// let s2="javascript developer and javascript course"
+// s2=s2.replace ("javascript","Js")
+// console.log(s2)
+
+
+// console.log(s2.indexOf("and"))
+// console.log(s2.slice(0,s2.indexOf("and")).trim())
+
+
+
+// // Validation to separate string
+// email="praveennaik0814@gmail.com"
+// let Validateemail=email.includes("@") && email.endswith(".com")
+// console.log(Validateemail)
+
+
+let name="praveen"
+let email="praveennaik0814@gmail.com"
+let bio="Javscript developer"
+
+console.log( `${name}, ${email}, ${bio}`)
 
